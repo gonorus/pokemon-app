b621df2c4b43c8b373ad0962b5e0437e985c176c
Dwi Anggono W.S.P <casperkepalabundar@gmail.com>
2021-08-27 19:26:42 +0700

    [DEV] added pokemon base status chart
    [DEV] added pokemon base status detail

---
+++ src/components/BaseStatusChart.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+/* @jsx jsx */
+import { jsx, css } from '@emotion/react';
+import RadarChart from 'react-svg-radar-chart';
+import 'react-svg-radar-chart/build/css/index.css'
+
+import { BASE_STATUS_TYPE } from './enums';
+
+const BaseStatusChart = (props) => {
+  const data = [
+    {
+      data: {
+        [BASE_STATUS_TYPE.AttackPoint]: props.attack/100,
+        [BASE_STATUS_TYPE.DefensePoint]: props.defense/100,
+        [BASE_STATUS_TYPE.SpAttackPoint]: props.specialAttack/100,
+        [BASE_STATUS_TYPE.SpDefensePoint]: props.specialDefense/100,
+        [BASE_STATUS_TYPE.SpeedPoint]: props.speed/100
+      },
+      meta: { color: 'blue' }
+    }
+  ];
+
+  const captions = {
+    [BASE_STATUS_TYPE.AttackPoint]: BASE_STATUS_TYPE.AttackPoint,
+    [BASE_STATUS_TYPE.DefensePoint]: BASE_STATUS_TYPE.DefensePoint,
+    [BASE_STATUS_TYPE.SpAttackPoint]: BASE_STATUS_TYPE.SpAttackPoint,
+    [BASE_STATUS_TYPE.SpDefensePoint]: BASE_STATUS_TYPE.SpDefensePoint,
+    [BASE_STATUS_TYPE.SpeedPoint]: BASE_STATUS_TYPE.SpeedPoint,
+  };
+
+  const StatusChartStyle = css({
+    '.caption': {
+      fontSize: '12px'
+    }
+  });
+  return (
+    <div css={StatusChartStyle}>
+      <RadarChart
+        captions={captions}
+        data={data}
+        size={250}
+      />
+    </div>
+  );
+};
+BaseStatusChart.propTypes = {
+  hp: PropTypes.number,
+  attack: PropTypes.number,
+  defense: PropTypes.number,
+  specialAttack: PropTypes.number,
+  specialDefense: PropTypes.number,
+  speed: PropTypes.number
+};
+BaseStatusChart.defaultProps = {
+  hp: 1000,
+  attack: 20,
+  defense: 40,
+  specialAttack: 60,
+  specialDefense: 80,
+  speed: 100
+};
+export default BaseStatusChart;
--- src/components/enums.js
+++ src/components/enums.js
@@ -2,6 +2,16 @@ const COMPONENT_HEIGHT = {
   Menubar: '50px',
 }
 
+const BASE_STATUS_TYPE = {
+  HealthPoint: 'Health Point',
+  AttackPoint: 'Attack',
+  DefensePoint: 'Defense',
+  SpAttackPoint: 'Sp. Attack',
+  SpDefensePoint: 'Sp. Defense',
+  SpeedPoint: 'Speed',
+}
+
 export {
   COMPONENT_HEIGHT,
+  BASE_STATUS_TYPE
 };
