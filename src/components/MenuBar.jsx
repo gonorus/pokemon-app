b9ddab253ba157747d1beadac807156ee0e5316c
Dwi Anggono W.S.P <casperkepalabundar@gmail.com>
2021-08-29 22:50:29 +0700

    [DEV] added menu for pokemon bag

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -3,37 +3,51 @@ import React from 'react';
 import { jsx, css } from '@emotion/react';
 import { Link } from 'react-router-dom';
 
-import PokemonLogoSVG from '../images/pokemon_logo.svg';
+import PokemonLogo from '../images/pokemon_logo.svg';
+import PokedexLogo from '../images/pokedex.svg';
 import { COMPONENT_HEIGHT, COMPONENT_MAX_WIDTH } from './enums';
 
 const MenuBar = () => {
   const MenuStyle = css({
-    padding: '0 16px',
     display: 'flex',
     justifyContent: 'center',
     backgroundColor: '#3B4CCA',
 
     'nav': {
       maxWidth: COMPONENT_MAX_WIDTH.Menubar,
+      flexGrow: 1,
       display: 'flex',
       flexDirection: 'row',
-      flexGrow: 1,
-      justifyContent: 'space-between',
-      height: COMPONENT_HEIGHT.Menubar
+      height: COMPONENT_HEIGHT.Menubar,
+      justifyContent: 'space-between'
     }
   });
 
   const LogoStyle = css({
     width: '150px',
-    backgroundImage: `url(${PokemonLogoSVG})`,
+    backgroundImage: `url(${PokemonLogo})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    color: 'transparent',
+
+    '@media (min-width: 600px)': {
+      marginLeft: '8px'
+    }
+  });
+
+  const PokedexStyle = css({
+    margin: '8px 16px',
+    backgroundImage: `url(${PokedexLogo})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     color: 'transparent'
   });
+
   return (
     <div css={MenuStyle}>
       <nav>
         <Link to='/' css={LogoStyle}>Home</Link>
+        <Link to='/bag' css={PokedexStyle}>Bag</Link>
       </nav>
     </div>
   );
